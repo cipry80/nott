4b0afa2315127f427ea199f2fd345c6d7b468d57
reign <the.reignn@gmail.com>
2018-01-18 16:18:40 +0200

    Remove append file, move to using streams. It is due to errors for too many open handles

--- server/routers/logs/logs.js
+++ server/routers/logs/logs.js
@@ -3,25 +3,25 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+const filePath = path.join(__dirname, '/logs/csp.txt');
+
 router.post('/report-csp-violation', (req, res) => {
   try {
     const data = req.body
       ? `
-      ********************
-      CSP Violation: ${JSON.stringify(req.body)}
-      ********************
-
-
-
+    ********************
+    CSP Violation: ${JSON.stringify(req.body)}
+    ********************
     `
       : `
     ********************
     CSP Violation: No data received!
     ********************
-    
-    
     `;
-    fs.appendFileSync(path.join(__dirname, '/logs/csp.txt'), data);
+
+    const stream = fs.createWriteStream(filePath, { flags: 'a' });
+    stream.write(data + '\n\n');
+    stream.end();
   } catch (err) {
     console.log(err);
   }
