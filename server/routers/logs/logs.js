47332ac58719c9ba8d2ee2b0df058d1b32966350
reign <the.reignn@gmail.com>
2018-01-18 16:29:10 +0200

    Export router + fix path + write on events to avoid race condition

--- server/routers/logs/logs.js
+++ server/routers/logs/logs.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
-const filePath = path.join(__dirname, '/logs/csp.txt');
+const filePath = path.join(__dirname, '../../logs/csp.txt');
 
 router.post('/report-csp-violation', (req, res) => {
   try {
@@ -20,11 +20,15 @@ router.post('/report-csp-violation', (req, res) => {
     `;
 
     const stream = fs.createWriteStream(filePath, { flags: 'a' });
-    stream.write(data + '\n\n');
-    stream.end();
+    stream.on('open', () => {
+      stream.write(data + '\n\n');
+      stream.end();
+    });
   } catch (err) {
     console.log(err);
   }
 
   res.status(204).end();
 });
+
+module.exports = router;
