db2cc708a9a2e078c4a49fb306f0f8d6e6da7cb7
reign <the.reignn@gmail.com>
2018-01-31 12:23:02 +0200

    prep for ant message usage

--- client/src/common/components/LoadingMsg/LoadingMsg.js
+++ client/src/common/components/LoadingMsg/LoadingMsg.js
@@ -2,8 +2,19 @@ import React, { PureComponent } from 'react';
 import message from 'antd/lib/message';
 import './LoadingMsg.css';
 
+/*
 const Error = () => message.error('Ups! Something went wrong.');
 const Warning = () => message.warning("We're still loading data for you...");
+*/
+
+const Error = () => (
+  <div className="loading-msg loading-error">Ups! Something went wrong.</div>
+);
+const Warning = () => (
+  <div className="loading-msg loading-delayed">
+    We're still loading data for you...
+  </div>
+);
 
 class LoadingMsg extends PureComponent {
   render = () => {
