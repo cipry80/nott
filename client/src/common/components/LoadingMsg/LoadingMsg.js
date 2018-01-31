00e7d63038875e90ba8d5e598f7cd5490600ffc1
reign <the.reignn@gmail.com>
2018-01-31 14:29:27 +0200

    remove msg from ant. it's API triggered only

--- client/src/common/components/LoadingMsg/LoadingMsg.js
+++ client/src/common/components/LoadingMsg/LoadingMsg.js
@@ -1,29 +1,22 @@
 import React, { PureComponent } from 'react';
-import message from 'antd/lib/message';
 import './LoadingMsg.css';
-
-/*
-const Error = () => message.error('Ups! Something went wrong.');
-const Warning = () => message.warning("We're still loading data for you...");
-*/
-
-const Error = () => (
-  <div className="loading-msg loading-error">Ups! Something went wrong.</div>
-);
-const Warning = () => (
-  <div className="loading-msg loading-delayed">
-    We're still loading data for you...
-  </div>
-);
 
 class LoadingMsg extends PureComponent {
   render = () => {
     const { error, pastDelay } = this.props;
 
     if (error) {
-      return <Error />;
+      return (
+        <div className="loading-msg loading-error">
+          Ups! Something went wrong.
+        </div>
+      );
     } else if (pastDelay) {
-      return <Warning />;
+      return (
+        <div className="loading-msg loading-delayed">
+          We're still loading data for you...
+        </div>
+      );
     } else {
       return null;
     }
